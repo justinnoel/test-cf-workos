c8ed1fea0cd6853790acaf7d07d1a4e0c0fe24ca
Justin Noel <81643826+justinnoel@users.noreply.github.com>
2024-06-13 07:57:12 -0500

    feat: add WorkOS

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-import { Hono } from 'hono'
+import { WorkOS } from "@workos-inc/node";
+import { Hono } from "hono";
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+  [key in keyof CloudflareBindings]: CloudflareBindings[key];
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  console.log(`c.env.WORKOS_API_KEY = ${c.env.WORKOS_API_KEY}`);
+  console.log(`c.env.WORKOS_CLIENT_ID = ${c.env.WORKOS_CLIENT_ID}`);
+  console.log(`c.env.WORKOS_CALLBACK = ${c.env.WORKOS_CALLBACK}`);
 
-export default app
+  const workos = new WorkOS(c.env.WORKOS_API_KEY);
+
+  const magicAuth = await workos.userManagement.createMagicAuth({
+    email,
+  });
+
+  console.log(`magicAuth = ${magicAuth}`);
+
+  return c.text("Hello Hono!");
+});
+
+export default app;
